edbffa641dcc96444bff19b997de050ffc9941c0
sassy watanabe <sassy.watanabe@gmail.com>
2012-01-09 19:11:57 +0900

    modify data type of model.

--- model/message.rb
+++ model/message.rb
@@ -7,7 +7,7 @@ class Message < Sequel::Model
   unless table_exists?
     set_schema do
       primary_key :id
-      string :name
+      text :name
       text :message
       timestamp :posted_date
     end
